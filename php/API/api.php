4f766e9e857d3f92625c03146018d4fc0eb18b8a
andres mesa <andres.mesa@lafactura.co>
2020-01-25 12:35:49 -0500

    Trabajando en el API

--- php/API/api.php
+++ php/API/api.php
@@ -4,40 +4,56 @@
   define('LF_METHOD_PUT',"PUT");
   define('LF_METHOD_DELETE',"DELETE");
 
-  function LF_send($url,$send,$usr,$pwd,$method=LF_METHOD_POST){
-    try{
-       $headers = array(
-            "Content-type: application/json",
-            "Cache-Control: no-cache",
-            "Pragma: no-cache",
-            "Content-Length: ".strlen($send),
-            "Connection: Keep-Alive",
-            "User-Agent: LaFactura.co/4.0",
-        );
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 50); //Espera 50 Secs
-        switch($method){
-          case LF_METHOD_PUT: curl_setopt($ch, CURLOPT_PUT, true);break;
-          case LF_METHOD_POST: curl_setopt($ch, CURLOPT_POST, true);break;
-          default: curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+  class LF_API_v200 {
+      public $error=false;
+      public $url,$usr,$pwd,$response;
+
+      public function __construct($url,$usr,$pwd){
+        $this->url=$url;
+        $this->usr=$usr;
+        $this->pwd=$pwd;
+      }
+
+      public function send($service,$send,$method=LF_METHOD_POST){
+        $this->error=false;
+        $this->response="";
+        try{
+            $headers = array(
+              "Content-type: application/json",
+              "Cache-Control: no-cache",
+              "Pragma: no-cache",
+              "Content-Length: ".strlen($send),
+              "Connection: Keep-Alive",
+              "User-Agent: LaFactura.co/4.0",
+            );
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+            curl_setopt($ch, CURLOPT_URL, $this->url."/".$service);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 50); //Espera 50 Secs
+            switch($method){
+              case LF_METHOD_PUT: curl_setopt($ch, CURLOPT_PUT, true);break;
+              case LF_METHOD_POST: curl_setopt($ch, CURLOPT_POST, true);break;
+              default: curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+            }
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $send);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_USERPWD, "{$this->usr}:{$this->pwd}");
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30); //Timeout
+            $response = curl_exec($ch);
+            $this->response=$response;
+            $curl_info= curl_getinfo($ch);
+            if(curl_error($ch)){
+              $this->error=true;
+              return null;
+            }
+            curl_close($ch);
+          } catch(Exception $e){
+            $this->error=true;
+            return null;
         }
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $send);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_USERPWD, "$usr:$pwd");
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30); //Timeout
-        $response = curl_exec($ch);
-        $curl_info= curl_getinfo($ch);
-        if(curl_error($ch)){
-          return false;
-        }
-        curl_close($ch);
-    } catch(Exception $e){
-        return false;
-    }
-    return $response;
+        return $response;
+      }
   }
 
 
---
+++ php/verifyStatus.php
@@ -0,0 +1,22 @@
+<?php
+  //Este ejemplo verifica el estado de una factura en el sistema
+
+  include_once("API/api.php");
+
+  $usuario="elusuario"; //coloca en este punto tu usuario
+  $contrasena="lacontrasena"; //coloca tu contraseña
+  $url="https://playground.lafactura.co/api2.0"; //estea esl la direccion de acceso a la api
+
+  //aqui se solicita el estado del documento en cuestion
+  $solicitud=[
+    "verifyStatus"=>[
+        "tascode"=>"fc-54984309jtr0ret90jjrg";
+    ];
+  ];
+
+  $api=new LF_API_v200($url,$usuario.$contrasena);
+  $api->send("invoice",json_encode($solicitud,JSON_UNESCAPED_UNICODE),LF_METHOD_POST);
+  echo $api->response; //resulado de la peticion en JSON
+
+
+?>
