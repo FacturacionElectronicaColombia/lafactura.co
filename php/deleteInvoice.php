d54dd4687dc99885fb846a6cbb59fa8f47cecf22
andres mesa <andres.mesa@lafactura.co>
2020-01-27 05:58:59 -0500

    API

---
+++ php/deleteInvoice.php
@@ -0,0 +1,30 @@
+<?php
+  //Este ejemplo anula factura con una nota credito por el valor total
+  include_once("API/api.php");
+
+  $usuario="elusuario"; //coloca en este punto tu usuario
+  $contrasena="lacontrasena"; //coloca tu contraseña
+  $url="https://playground.lafactura.co/api2.0"; //estea esl la URL de acceso a la api
+
+  date_default_timezone_set('America/Bogota'); //coloco la hora para colombia
+
+  $tascode="fc-54984309jtr0ret90jjrg"; //factura a anular
+  $hash=md5($tascode.date("Ymd")); //genero el hash (md5)
+
+  //aqui se prepara la informacion a ser enviada
+  $solicitud=[
+    "deleteInvoice"=>[
+        "tascode"=>$tascode, //factura a anular
+        "hash"=>$hash, //hash creado
+        "description"=>"El cliente no aceptó el producto" //motivo de anulacion
+    ]
+  ];
+  //convierto a JSON la solicitud
+  $solicitud=json_encode($solicitud,JSON_UNESCAPED_UNICODE | JSON_HEX_QUOT);
+
+  $api=new LF_API_v200($url,$usuario,$contrasena);
+  $api->send(LF_SERVICE_INVOICE,$solicitud,LF_METHOD_DELETE);
+  echo $api->response; //resulado de la peticion en JSON
+
+
+?>
