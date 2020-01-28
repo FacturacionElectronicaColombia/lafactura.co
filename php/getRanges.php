3849d001e9d245a9665c32445d9492ce4714b0bc
Andres Mesa <andres.mesa@lafactura.co>
2020-01-28 18:59:11 -0500

    API

--- php/getRanges.php
+++ php/getRanges.php
@@ -1,24 +1,24 @@
 <?php
-  //Este ejemplo verifica el estado de un documento en el sistema
+    //Este ejemplo recibe todos los rangos disponibles para documentos
 
-  include_once("API/api.php");
+    include_once("API/api.php");
 
-  $usuario="elusuario"; //coloca en este punto tu usuario
-  $contrasena="lacontrasena"; //coloca tu contraseña
-  $url="https://playground.lafactura.co/api2.0"; //estea esl la URL de acceso a la api
+    $usuario="elusuario"; //coloca en este punto tu usuario
+    $contrasena="lacontrasena"; //coloca tu contraseña
+    $url="https://playground.lafactura.co/api2.0"; //estea esl la URL de acceso a la api
 
-  //aqui se solicita el estado del documento en cuestion -formato arreglo
-  $solicitud=[
-    "getRanges"=>[
-        "mode"=>"active",
-        "type"=>"invoice"
-    ]
-  ];
-  //convierto a JSON la solicitud
-  $solicitud=json_encode($solicitud,JSON_UNESCAPED_UNICODE | JSON_HEX_QUOT);
+    //solicita los rangos de documentos
+    $solicitud=[
+      "getRanges"=>[
+          "mode"=>"active", //rangos activos o inactivos
+          "type"=>"all" //todos o pueden ser invoice, creditNote, debitNote, all (todos)
+      ]
+    ];
+    //convierto a JSON la solicitud
+    $solicitud=json_encode($solicitud,JSON_UNESCAPED_UNICODE | JSON_HEX_QUOT);
 
-  $api=new LF_API_v200($url,$usuario,$contrasena);
-  $api->send(LF_SERVICE_GENERAL,$solicitud,LF_METHOD_GET); //metodo GET
-  echo $api->response; //resulado de la peticion en JSON
+    $api=new LF_API_v200($url,$usuario,$contrasena);
+    $api->send(LF_SERVICE_GENERAL,$solicitud,LF_METHOD_GET); //metodo GET
+    echo $api->response; //resulado de la peticion en JSON
 
 ?>
