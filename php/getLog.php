69c0861b978ee60a272812a4b0d96ea8bc10fcfb
Andres Mesa <andres.mesa@lafactura.co>
2020-01-28 09:47:53 -0500

    API

---
+++ php/getLog.php
@@ -0,0 +1,25 @@
+<?php
+  //Este ejemplo verifica el estado de un documento en el sistema
+
+  include_once("API/api.php");
+
+  $usuario="elusuario"; //coloca en este punto tu usuario
+  $contrasena="lacontrasena"; //coloca tu contraseña
+  $url="https://playground.lafactura.co/api2.0"; //estea esl la URL de acceso a la api
+
+  $tascode="fc-54984309jtr0ret90jjrg"; //documento a verificar
+
+  //aqui se solicita el estado del documento en cuestion -formato arreglo
+  $solicitud=[
+    "getLog"=>[
+        "tascode"=>$tascode
+    ]
+  ];
+  //convierto a JSON la solicitud
+  $solicitud=json_encode($solicitud,JSON_UNESCAPED_UNICODE | JSON_HEX_QUOT);
+
+  $api=new LF_API_v200($url,$usuario,$contrasena);
+  $api->send(LF_SERVICE_INVOICE,$solicitud,LF_METHOD_GET); //metodo GET
+  echo $api->response; //resulado de la peticion en JSON
+
+?>
