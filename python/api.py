1d85ae83fbe456beee0b27147107b34bdc33b24a
Andres Mesa <andres.mesa@lafactura.co>
2020-08-13 11:32:24 -0500

    Comentarios

--- python/api.py
+++ python/api.py
@@ -6,19 +6,19 @@ LF_INVOICE="/invoice/index.php"
 
 def getInfo_LF(url, service, method, usr, pwd, info):
   return requests.request(method,url+service,json=info,headers={"content-type": "application/json"},auth=(usr,pwd))
-
+# Funcion para obtener rangos
 def getRanges(url,usr,pwd,info):
     return getInfo_LF(url,LF_GENERAL,"GET",usr,pwd,info)
-
+# Funcion para enviar una factura
 def invoice(url,usr,pwd,info):
     return getInfo_LF(url,LF_INVOICE,"POST",usr,pwd,info)
-
+# Funcion para obtener el log de un documento
 def getLog(url,usr,pwd,info):
     return getInfo_LF(url,LF_INVOICE,"GET",usr,pwd,info)
-
+# Funcion para  verificar el estado de un documento
 def verifyStatus(url,usr,pwd,info):
     return getInfo_LF(url,LF_INVOICE,"GET",usr,pwd,info)
-
+# Funcion para anular una factura
 def deleteInvoice(url,usr,pwd,info):
     return getInfo_LF(url,LF_INVOICE,"DELETE",usr,pwd,info)
 
