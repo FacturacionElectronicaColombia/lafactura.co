805172f642dde9915fb2bdbf3fb8f5ef6fa02504
Andres Mesa <andres.mesa@lafactura.co>
2020-08-20 16:42:54 -0500

    Creacion ejemplo de RUES

---
+++ php/registroMercantil.php
@@ -0,0 +1,23 @@
+<?php
+  //Este ejemplo permite reenviar un correo con el documento solicitado
+
+  include_once("API/api.php");
+
+  $usuario="elusuario"; //coloca en este punto tu usuario
+  $contrasena="secretas"; //coloca tu contraseña
+  $url="https://playground.lafactura.co/api2.0"; //estea esl la URL de acceso a la api
+
+  //solicita envio de correo
+  $solicitud=[
+    "registroMercantil"=>[
+        "id"=>"900900900" //documento en cuestion
+    ]
+  ];
+  //convierto a JSON la solicitud
+  $solicitud=json_encode($solicitud,JSON_UNESCAPED_UNICODE | JSON_HEX_QUOT);
+
+  $api=new LF_API_v200($url,$usuario,$contrasena);
+  $api->send(LF_SERVICE_GENERAL,$solicitud,LF_METHOD_GET); //metodo POST
+  echo $api->response; //resulado de la peticion en JSON
+
+?>
