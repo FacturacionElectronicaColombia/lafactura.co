2d42e1875fee320f1f3c3413f2d7c60760488940
Andres Mesa <andres.mesa@lafactura.co>
2020-08-20 16:59:20 -0500

    Ejemplo registro mercantil

--- python/api.py
+++ python/api.py
@@ -9,8 +9,11 @@ def getInfo_LF(url, service, method, usr, pwd, info):
 # Funcion para obtener rangos
 def getRanges(url,usr,pwd,info):
     return getInfo_LF(url,LF_GENERAL,"GET",usr,pwd,info)
-# Funcion para enviar emails
+# Funcion para obtuener email de la DIAN
 def getEmail(url,usr,pwd,info):
+    return getInfo_LF(url,LF_GENERAL,"GET",usr,pwd,info)
+# Funcion para obtuener el registro mercantl del RUES
+def registroMercantil(url,usr,pwd,info):
     return getInfo_LF(url,LF_GENERAL,"GET",usr,pwd,info)
 #Envio de email a cliente
 def sendEmail(url,usr,pwd,info):
